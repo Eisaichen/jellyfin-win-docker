07c2f7985427cf640dda368b4161ba1233808e95
eisaichen <eisaichen>
2024-08-16 18:06:01 -0500

    Init

---
+++ build.ps1
@@ -0,0 +1,9 @@
+.\wget --no-hsts "https://repo.jellyfin.org/files/server/windows/latest-stable/amd64/jellyfin_$($env:GH_CI_TAG.substring(1))-amd64.zip" -O .\jellyfin.zip
+Expand-Archive -Path .\jellyfin.zip -DestinationPath .\build\
+
+docker pull mcr.microsoft.com/windows/server:ltsc2022
+docker build --isolation hyperv --no-cache -t eisai/jellyfin-nvidia:latest -t eisai/jellyfin-nvidia:$env:GH_CI_TAG .\build
+
+if ($env:GH_CI_PUSH -eq "false") {
+    docker push eisai/jellyfin-nvidia -a
+}
