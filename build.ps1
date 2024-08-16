5b63db026b6414e902ec83d86c38cf9ed5e70d29
Eisai <12467320+Eisaichen@users.noreply.github.com>
2024-08-16 18:59:03 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-.\wget --no-hsts "https://repo.jellyfin.org/files/server/windows/latest-stable/amd64/jellyfin_$($env:GH_CI_TAG.substring(1))-amd64.zip" -O .\jellyfin.zip
+.\wget --no-hsts -q "https://repo.jellyfin.org/files/server/windows/latest-stable/amd64/jellyfin_$($env:GH_CI_TAG.substring(1))-amd64.zip" -O .\jellyfin.zip
 Expand-Archive -Path .\jellyfin.zip -DestinationPath .\build\
 
 docker pull mcr.microsoft.com/windows/server:ltsc2022
