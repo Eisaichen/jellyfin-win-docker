ab61f4bcc7bdd553fa5fa00c966db388c82b4746
eisaichen <eisaichen>
2025-01-02 04:06:27 -0600

    Simplify

--- build.ps1
+++ build.ps1
@@ -1,11 +1,10 @@
 .\wget --no-hsts -q "https://repo.jellyfin.org/files/server/windows/latest-stable/amd64/jellyfin_$($env:GH_CI_TAG.substring(1))-amd64.zip" -O .\jellyfin.zip
 Expand-Archive -Path .\jellyfin.zip -DestinationPath .\build\
 
-docker pull mcr.microsoft.com/windows/server:ltsc2022
 if ($env:GH_CI_PUSH -eq "true") {
-    docker build --isolation hyperv --no-cache -t eisai/jellyfin-nvidia:latest -t eisai/jellyfin-nvidia:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --no-cache --pull -t eisai/jellyfin-nvidia:latest -t eisai/jellyfin-nvidia:$env:GH_CI_TAG .\build
 } else {
-    docker build --isolation hyperv --no-cache -t eisai/jellyfin-nvidia:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --no-cache --pull -t eisai/jellyfin-nvidia:$env:GH_CI_TAG .\build
 }
 
 if ($env:GH_CI_PUSH -eq "true") {
